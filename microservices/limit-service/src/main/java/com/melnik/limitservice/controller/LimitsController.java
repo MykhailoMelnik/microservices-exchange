949b6a957cadf88d7c5279b7ff99ca07d4452103
mihajlomelnik <melnik8686@gmail.com>
2024-02-24 15:38:05 +0200

    add connect git settings limits

--- microservices/limit-service/src/main/java/com/melnik/limitservice/controller/LimitsController.java
+++ microservices/limit-service/src/main/java/com/melnik/limitservice/controller/LimitsController.java
@@ -16,6 +16,5 @@ public class LimitsController {
 	public Limits retrieveLimits() {
 		return new Limits(configuration.getMinimum(), 
 				configuration.getMaximum());
-//		return new Limits(1,1000);
 	}
 }
